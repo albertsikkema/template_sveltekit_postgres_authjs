ef806d62ed239d2983bd0c76c03ea83344160c4a
Albert Sikkema <a.sikkema@epblc.nl>
2025-03-18 14:37:01 +0100

    new migration

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.js',
-	out: './drizzle',
+	out: './.drizzle/migrations',
 
 	dbCredentials: {
 		url: process.env.DATABASE_URL
